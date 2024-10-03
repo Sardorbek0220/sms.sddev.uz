423f3cee3fdeddfa879c53d4af463f84a0e98fbd
sardor <sardorbek0220roberto@gmail.com>
2024-10-03 17:59:42 +0500

    saving frameworks

--- resources/views/admin/monitoring.php
+++ resources/views/admin/monitoring.php
@@ -2,14 +2,13 @@
 <html lang="en">
 <head>
 	<meta name="robots" content="noindex">
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 	<link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
   	<link href="https://cdn.jsdelivr.net/npm/@mdi/font@6.x/css/materialdesignicons.min.css" rel="stylesheet">
-  	<link href="https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.min.css" rel="stylesheet">
-	<script src="https://test17.salesdoc.io/js/axios.min.js"></script>
+  	<link href="/assets/other/bootstrap.min.css" rel="stylesheet">
+  	<link href="/assets/other/vuetify.min.css" rel="stylesheet">
 	<meta charset="UTF-8">
 	<link rel="icon" href="../assets/logo.png">
 	<title>Monitoring | Sales Doctor</title>
@@ -548,8 +547,10 @@
 
 </div>
 
-<script src="https://cdn.jsdelivr.net/npm/vue@2.x/dist/vue.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.js"></script>
+<script src="/assets/other/axios.min.js"></script>
+<script src="/assets/other/vue.js"></script>
+<script src="/assets/other/vuetify.js"></script>
+
 <script>
 
 	const statusColors = {
