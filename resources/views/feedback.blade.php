5ddcf195db4c8840900e8de03518697a211c65ef
sardor <sardorbek0220roberto@gmail.com>
2025-02-10 11:31:01 +0500

    mark4

--- resources/views/feedback.blade.php
+++ resources/views/feedback.blade.php
@@ -80,6 +80,16 @@
                 </div>
               </div>
 
+              <div class="card text-white mb-5 beforeCheck" style="width: 100%; background-color: #FFA500; cursor:pointer" onclick="send('4')">
+                <div class="card-header">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-emoji-neutral" viewBox="0 0 16 16">
+                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+                    <path d="M4 10.5a.5.5 0 0 0 .5.5h7a.5.5 0 0 0 0-1h-7a.5.5 0 0 0-.5.5m3-4C7 5.672 6.552 5 6 5s-1 .672-1 1.5S5.448 8 6 8s1-.672 1-1.5m4 0c0-.828-.448-1.5-1-1.5s-1 .672-1 1.5S9.448 8 10 8s1-.672 1-1.5"/>
+                  </svg>&nbsp&nbsp
+                  <span style="font-size: 24px">Дастурда хатолик</span>
+                </div>
+              </div>
+
               <div class="form-group mt-3 afterCheck" style="display: none;">
                 <textarea type="text" class="form-control" name="complaint" id="complaint" placeholder="Изох"></textarea>
               </div>
