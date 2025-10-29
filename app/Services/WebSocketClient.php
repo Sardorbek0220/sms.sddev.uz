893150c2634c42bb9c653e2fd16d91c06632d465
Sardor <sardorbek0220roberto@gmail.com>
2025-10-29 16:16:32 +0500

    fix

--- app/Services/WebSocketClient.php
+++ app/Services/WebSocketClient.php
@@ -10,6 +10,7 @@ use App\Operator_time;
 class WebSocketClient
 {
     protected $loop;
+    protected $attemps = 0;
 
     public function __construct()
     {
@@ -41,42 +42,43 @@ class WebSocketClient
                     $data = json_decode($msg);
                     // info("Received: {$msg}\n");
                     if ($data->event == 'user_registration') {
-                        // info("Received: {$msg}\n");
-                        // if (in_array($data->data->ip, ['84.54.117.196', '178.218.201.191'])) {
-                            if ($data->data->state == 'register') {
-                                $exist = Operator_time::where('uid', $data->data->uid)->where('port', $data->data->port)->where('ip', $data->data->ip)->where('unregister', 0)->where('created_at', '>', date('Y-m-d'))->first();
-                                if ($exist == null) {
-                                    $operator_time = Operator_time::create([
-                                        'uid' => $data->data->uid,
-                                        'register' => 1,
-                                        'unregister' => 0,
-                                        'ip' => $data->data->ip,
-                                        'port' => $data->data->port,
-                                        'timestamp_reg' => $data->data->date,
-                                    ]);
-                                    $operator_time->save();
-                                }
-                            }else if($data->data->state == 'unregister'){
-                                $operator_time = Operator_time::where('uid', $data->data->uid)->where('port', $data->data->port)->where('ip', $data->data->ip)->orderBy('timestamp_reg', 'desc')->first();
-                                if($operator_time != null){
-                                    $operator_time->unregister = 1;
-                                    $operator_time->timestamp_unreg = $data->data->date;
-                                    $operator_time->save();
-                                }
-                                
+                        
+                        if ($data->data->state == 'register') {
+                            $exist = Operator_time::where('uid', $data->data->uid)->where('port', $data->data->port)->where('ip', $data->data->ip)->where('unregister', 0)->where('created_at', '>', date('Y-m-d'))->first();
+                            if ($exist == null) {
+                                $operator_time = Operator_time::create([
+                                    'uid' => $data->data->uid,
+                                    'register' => 1,
+                                    'unregister' => 0,
+                                    'ip' => $data->data->ip,
+                                    'port' => $data->data->port,
+                                    'timestamp_reg' => $data->data->date,
+                                ]);
+                                $operator_time->save();
                             }
-                        // }
+                        }else if($data->data->state == 'unregister'){
+                            $operator_time = Operator_time::where('uid', $data->data->uid)->where('port', $data->data->port)->where('ip', $data->data->ip)->orderBy('timestamp_reg', 'desc')->first();
+                            if($operator_time != null){
+                                $operator_time->unregister = 1;
+                                $operator_time->timestamp_unreg = $data->data->date;
+                                $operator_time->save();
+                            }
+                            
+                        }
                     }
                     
                 });
 
                 $conn->on('close', function () {
                     info("Connection closed\n");
-                    echo "Connection closed\n";
+                    if ($this->attemps < 3) {
+                        $this->connect();
+                        info("attemp -> {$this->attemps}\n");
+                        $this->attemps++;
+                    }
                 });
             }, function ($e) {
                 info("Could not connect: {$e->getMessage()}\n");
-                echo "Could not connect: {$e->getMessage()}\n";
             });
 
         $this->loop->run();
